1eb7c1cdde43090b000b05d289166619ddd73886
Victoria de Astoreca <victoriadeastoreca@yahoo.com>
2022-07-25 22:56:04 -0300

    agregando archivos

---
+++ index.js
@@ -0,0 +1,43 @@
+// IDEA: preguntarle al usuario los productos que le gustaría ver (collar, aros, body chain, carteras). Decirle si están disponibles. 
+
+const nombre = prompt("Ingrese su nombre, por favor");
+console.log(nombre)
+
+const productoElegido = prompt(`¡Hola ${nombre}! Bienvenido a la tienda online de Lulu´s. ¿Qué producto le gustaría ver? Tenemos desde collares y aros, hasta carteras`)
+console.log(productoElegido)
+
+let collares = [ 
+    {nombre: "Choker basic white", disponibilidad: "disponible"},
+    {nombre: "Choker AIRE", disponibilidad: "disponible"},
+    {nombre: "Choker DAISY", disponibilidad: "disponible"},
+    {nombre: "Choker DUALITY", disponibilidad: "disponible"},
+    {nombre: "Choker JUPITER", disponibilidad: "no disponible"},
+    {nombre: "Choker MAUI", disponibilidad: "disponible"},
+    {nombre: "Collar BUTTERFLY", disponibilidad: "no disponible"},
+    {nombre: "Collar BUBBLE", disponibilidad: "disponible"},
+    {nombre: "Collar DAISY pastel", disponibilidad: "no disponible"}
+]
+let aros = [ 
+    {nombre: "Aros iris", disponibilidad: "disponible"},
+    {nombre: "Aros MOON", disponibilidad: "disponible"},
+    {nombre: "Aros DAISY", disponibilidad: "no disponible"}
+]
+
+function catalogo (){
+    if (productoElegido == "collar"){
+        const productosDisponibles = collares.filter((elemento) => {
+            return elemento.disponibilidad === "disponible"
+        })
+        console.log(productosDisponibles)
+    } else if (productoElegido == "aros"){
+        const productosDisponibles = aros.filter((elemento) => {
+            return elemento.disponibilidad === "disponible"
+        })
+        console.log(productosDisponibles)
+    } else if (productoElegido == "cartera"){
+        alert ("El producto ingresado solo se trabaja por encargo. Si está interesado en pedir uno, por favor contáctese a través de nuestro instagram: @lulusba")
+    } else {
+        alert ("Lo sentimos, pero el producto ingresado no está disponible o no es válido. Por favor, intente de nuevo.")
+    }
+}
+catalogo (productoElegido)
